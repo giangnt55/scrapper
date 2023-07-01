03232fab7a6eaaf4815f02cd074386bd12968c3f
Truong Giang <giangntse150747@gmail.com>
2023-07-01 23:24:25 +0700

    ok

--- action.js
+++ action.js
@@ -10,3 +10,23 @@ document.onreadystatechange = function() {
         navbar.classList.remove('navbar-dark');
     });
   }
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var scrollLinks = document.getElementsByClassName('scroll-link');
+    
+    for (var i = 0; i < scrollLinks.length; i++) {
+      scrollLinks[i].addEventListener('click', scrollToSection);
+    }
+    
+    function scrollToSection(event) {
+      event.preventDefault();
+      var targetId = this.getAttribute('href');
+      var targetSection = document.querySelector(targetId);
+      
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  });
+  
